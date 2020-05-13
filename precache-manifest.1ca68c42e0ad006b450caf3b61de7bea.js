9fb1356a4c023a08fa7f2c2f1d5a350183c3fe3c
Abhijeet Palanki <ajpalanki@gmail.com>
2020-05-13 17:25:37 -0500

    Updates

---
+++ precache-manifest.1ca68c42e0ad006b450caf3b61de7bea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "53ffa9ed69e6d795c076",
+    "url": "/me/static/js/main.53ffa9ed.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "53ffa9ed69e6d795c076",
+    "url": "/me/static/css/main.757c6d16.chunk.css"
+  },
+  {
+    "revision": "585fe481b78421bdbf04ca46417f328e",
+    "url": "/me/index.html"
+  }
+];
