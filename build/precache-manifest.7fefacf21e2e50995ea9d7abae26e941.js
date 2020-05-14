c56f8be1ffc9ab5a3f99a889d38adf3c74bb8717
Abhijeet Palanki <ajpalanki@gmail.com>
2020-05-13 23:03:08 -0500

    minor UI changes

---
+++ build/precache-manifest.7fefacf21e2e50995ea9d7abae26e941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "e6bf9730c1b8da475755",
+    "url": "/me/static/js/main.e6bf9730.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e6bf9730c1b8da475755",
+    "url": "/me/static/css/main.757c6d16.chunk.css"
+  },
+  {
+    "revision": "e283152518eaeb1f4f4edc445b8f1bf8",
+    "url": "/me/index.html"
+  }
+];
